252aa8be9ac01c8f9494caa7eec2b8ad2b12887b
alegonzalez <aoalvaradog@est.utn.ac.cr>
2016-03-03 23:18:30 -0600

    create method authentication for user, set the token and creation datetime

--- intercambealo/app/controllers/users_controller.rb
+++ intercambealo/app/controllers/users_controller.rb
@@ -1,25 +1,48 @@
 class UsersController < ApplicationController
-	skip_before_filter :verify_authenticity_token, only: [:create,:destroy]
+	#before_action :authenticate, only: [:show,:edit,:update,:destroy]
+	skip_before_filter :verify_authenticity_token, only: [:create,:destroy,:authenticate]
+
 	def index
 		user  = User.all;
 		render json: user, status: 200
 	end
 	#create a new user
+
 	def create
+		
 		user = User.new(params_user)
-		user.encryptionPassword(user)
-		if user.save
-			render nothing: true, status: 201
+		#if user.verificationUserNotRepit(user)
+		#	render json: user.errors, status: 422
+		#else
+			user.encryptionPassword(user)
+			if user.save
+				render nothing: true, status: 201
+			else
+				render json: user.errors, status: 422
+			end	
+		#end
+	end
+
+	#authenticate the user 
+	
+	def authenticate
+		user = User.new(params_authenticate)
+		
+		if user.valid_user_password(user)
+			render json: user , status: 200
 		else
-			render json: user.errors, status: 422
-		end	
+			render json: "Sorry the user not exist", status: 422
+		end
 	end
     #Allow only add 
     private
     def params_user
+    	
     	params.require(:user).permit(:username,:password,:firstname,:token,:creationDate)
     end
-
-
-
+    private 
+    def params_authenticate
+    	params.require(:user).permit(:username,:password)
+    end
+    
 end
