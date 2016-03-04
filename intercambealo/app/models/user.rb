e7bb140dfdbe7ddbd2968ffc08396b01deb2ee69
alegonzalez <aoalvaradog@est.utn.ac.cr>
2016-03-03 23:23:42 -0600

    creation of method for authentication and encrypt user password

--- intercambealo/app/models/user.rb
+++ intercambealo/app/models/user.rb
@@ -1,12 +1,35 @@
 class User < ActiveRecord::Base
 	require 'digest/md5'
-	
+	#validates :username,:password presence: true
     #:token,:creationDate
+    #set encryption of the password
     def encryptionPassword(user)
     	user.password = Digest::MD5.hexdigest(user.password)
     end
-    def setToken(user)
-    	#user.token = Digest::MD5.hexdigest(user.creationDate)
-    	user.creationDate = DateTime.time.to_formatted_s(:rfc822)
+    #verification that the user not this repeated
+   # def verificationUserNotRepit(user)
+    #  user = searchUserByUsername(user)
+    # if user.empty?
+     # error.add(:user,"The username are repeated")
+   #end
+end
+    #set encryption the date
+    def encryptionDateForToken(user,userSearch)
+    	user.creationDate = Time.now
+        user.token = Digest::MD5.hexdigest(user.username)
+        userSearch.update(:token => user.token, :creationDate =>user.creationDate)
     end
+
+    #validate that password and user are equals
+    def valid_user_password(user)
+       user.password = encryptionPassword(user)
+       userSearch =  searchUserByUsername(user)
+
+       if userSearch.password == user.password
+         encryptionDateForToken(user,userSearch)
+     end 
+ end
+ def searchUserByUsername (user)
+   User.find_by(username: user.username)     
+end
 end
