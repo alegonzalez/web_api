b3f5e4e131e77b1e12f6a144e33bc0182711db56
alegonzalez <aoalvaradog@est.utn.ac.cr>
2016-03-27 00:51:37 -0600

    Creacion de la los usuarios y autenticación

--- intercambealo/app/controllers/users_controller.rb
+++ intercambealo/app/controllers/users_controller.rb
@@ -1,48 +1,74 @@
 class UsersController < ApplicationController
+ require 'time'
+ before_action :set_users, only: [:destroy,:update]
+ before_action :validate_token, only: [:create,:update,:destroy]
 	#before_action :authenticate, only: [:show,:edit,:update,:destroy]
-	skip_before_filter :verify_authenticity_token, only: [:create,:destroy,:authenticate]
-
-	def index
-		user  = User.all;
-		render json: user, status: 200
-	end
+  skip_before_filter :verify_authenticity_token, only: [:create,:destroy,:authenticate,:update]
 	#create a new user
 
 	def create
-		
 		user = User.new(params_user)
-		#if user.verificationUserNotRepit(user)
-		#	render json: user.errors, status: 422
-		#else
+		user.verificationUserNotRepit(user)
+
+		if !user.errors{:username}.empty?
+			render json:  user.errors, status: 422	
+		else
 			user.encryptionPassword(user)
 			if user.save
 				render nothing: true, status: 201
 			else
 				render json: user.errors, status: 422
 			end	
-		#end
-	end
-
-	#authenticate the user 
-	
-	def authenticate
-		user = User.new(params_authenticate)
-		
-		if user.valid_user_password(user)
-			render json: user , status: 200
-		else
-			render json: "Sorry the user not exist", status: 422
 		end
 	end
-    #Allow only add 
+    #update user
+    def update
+    	if @user.update(params_user)
+    		render json: {"Message" => "Edit an specific user"},status: 200
+    	else
+    		render json: {"Message" => "The user can't edit"},status: 404
+    	end
+    end
+  	#Delete users
+  	def destroy
+  		if @user.destroy
+  			render json: {"Message" => "Deletes an specific user"}, status: 204
+  		else
+  			render json: {"Error" => "The user can't delete"}, status: 422
+  		end
+  	end
+   #authenticate the user 
+   def authenticate
+   	@user = User.new(params_authenticate)
+   	@user.valid_user_password(@user)
+   	if @user.errors{:password}.empty? || @user.errors{:username}.empty?
+   		session[:token] = @user.token
+       session[:expires] = Time.now + 1.minute
+       render json: {"Token" => session[:token]}, status: 200
+     else
+       render json: user.errors, status: 422
+     end
+   end
+   #validate the token 
+   def validate_token
+
+    if(session[:expires]<Time.now)
+      render json: {"Message" => "the session has expired, please log"} ,status: 402
+    end
+  end
+    #get data the user 
     private
     def params_user
-    	
-    	params.require(:user).permit(:username,:password,:firstname,:token,:creationDate)
+    	params.permit(:username,:password,:firstname,:token,:creationDate)
     end
+    #Allow only the params indicated
     private 
     def params_authenticate
-    	params.require(:user).permit(:username,:password)
+     params.require(:user).permit(:username,:password)
+   end
+    #search by id
+    private 
+    def set_users
+    	@user = User.find(params[:id])
     end
-    
-end
+  end
--- intercambealo/app/models/user.rb
+++ intercambealo/app/models/user.rb
@@ -1,35 +1,44 @@
 class User < ActiveRecord::Base
-	require 'digest/md5'
-	#validates :username,:password presence: true
+	require 'digest/md5'  
+	validates :username, presence: {:message => "The camp name is required"}
+  validates :password, presence: {:message => "The camp password is required"}
+  #validates :firsname, presence: {:message => "The camp firsname is required"}
     #:token,:creationDate
     #set encryption of the password
     def encryptionPassword(user)
     	user.password = Digest::MD5.hexdigest(user.password)
     end
     #verification that the user not this repeated
-   # def verificationUserNotRepit(user)
-    #  user = searchUserByUsername(user)
-    # if user.empty?
-     # error.add(:user,"The username are repeated")
-   #end
-end
+    def verificationUserNotRepit(user)
+      userp = searchUserByUsername(user)
+      if !userp.nil?
+      user.errors.add(:username,"The user is repeated, please introdeced other")
+      end
+    end
+   
     #set encryption the date
     def encryptionDateForToken(user,userSearch)
     	user.creationDate = Time.now
-        user.token = Digest::MD5.hexdigest(user.username)
-        userSearch.update(:token => user.token, :creationDate =>user.creationDate)
+      user.token = user.creationDate #Digest::MD5.hexdigest(user.creationDate.to_s)
+      userSearch.update(:token => user.token, :creationDate => user.creationDate)
     end
 
     #validate that password and user are equals
     def valid_user_password(user)
-       user.password = encryptionPassword(user)
-       userSearch =  searchUserByUsername(user)
-
+     user.password = encryptionPassword(user)
+     userSearch =  searchUserByUsername(user)
+     if !userSearch.nil?
        if userSearch.password == user.password
          encryptionDateForToken(user,userSearch)
-     end 
- end
- def searchUserByUsername (user)
+       else
+        user.errors.add(:password,"The password is not valid")
+      end 
+    else
+      user.errors.add(:username,"The username is not valid")
+      user.errors.add(:password,"The password is not valid")
+    end
+  end
+  def searchUserByUsername (user)
    User.find_by(username: user.username)     
-end
+ end
 end
